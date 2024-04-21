33c2ab966b85ce1fd285c5223f4b7a92375e3f60
Baptiste GOULARD <bgoulard@student.42.fr>
2024-04-21 15:19:51 +0200

    [norming] norming of ft_args

---
+++ include/ft_defs.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_defs.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bgoulard <bgoulard@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/21 14:55:34 by bgoulard          #+#    #+#             */
+/*   Updated: 2024/04/21 15:01:52 by bgoulard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_DEFS_H
+# define FT_DEFS_H
+
+# include "ft_map_types.h"
+# include "ft_args_types.h"
+# include "ft_list_types.h"
+# include "ft_vector_types.h"
+# include "ft_optional_types.h"
+# include <stdbool.h>
+# include <stddef.h>
+
+typedef int	(*t_funcmp)(const void *, const void *);
+
+#endif /* FT_DEFS_H */
--- src/ft_args/ft_progname.c
+++ src/ft_args/ft_progname.c
@@ -6,23 +6,23 @@
 /*   By: bgoulard <bgoulard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/14 00:00:49 by bgoulard          #+#    #+#             */
-/*   Updated: 2024/04/21 10:22:03 by bgoulard         ###   ########.fr       */
+/*   Updated: 2024/04/21 14:44:46 by bgoulard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_args.h"
 #include <stddef.h>
 
-static const char *singleton_progname(const char *progname)
+static const char	*singleton_progname(const char *progname)
 {
-	static const char *singleton_progname = NULL;
+	static const char	*singleton_progname = NULL;
 
 	if (progname)
 		singleton_progname = progname;
 	return (singleton_progname);
 }
 
-void ft_set_progname(const char *progname)
+void	ft_set_progname(const char *progname)
 {
 	if (progname)
 		singleton_progname(progname);
@@ -30,8 +30,7 @@ void ft_set_progname(const char *progname)
 		singleton_progname("a.out");
 }
 
-const char *ft_progname(void)
+const char	*ft_progname(void)
 {
 	return (singleton_progname(NULL));
 }
-
