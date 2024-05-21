674228635ef8e709b11e8fa93ce5cd6163d7d155
Baptiste GOULARD <bgoulard@student.42.fr>
2024-05-21 10:51:22 +0200

    arg parser on the way

--- include/ft_args_types.h
+++ include/ft_args_types.h
@@ -6,66 +6,134 @@
 /*   By: bgoulard <bgoulard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/13 23:42:58 by bgoulard          #+#    #+#             */
-/*   Updated: 2024/05/18 16:27:12 by bgoulard         ###   ########.fr       */
+/*   Updated: 2024/05/21 09:36:27 by bgoulard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_ARGS_TYPES_H
 # define FT_ARGS_TYPES_H
 
+# define ARG_MASK_ATYPE 0x1C
+# define ARG_MASK_ANY_ARG 0x1
+
 /// @brief Enum to define the type of the option
-/// @details The type of the option will define how the argument of the option
-/// will be parsed. You can use the following as masks:
-/// OPT_NOARG: The option does not have any argument
-/// OPT_EQSIGN: The option has an argument that is separated by an equal sign
-/// OPT_SPACE: The option has an argument that is separated by a space
+/// @details The type of the option is defined by the flags that are set in the
+/// t_opt structure. The flags are defined in the enum e_opt_type.
+/// The flags are defined as follows:
+/// - OPT_ARG: The option has an argument
+/// - OPT_EQSIGN: The option has an argument and the argument is separated by an
+/// equal sign '=' (e.g. --option=arg). If the flag is not set, the argument is
+/// considered to be in the next argument (e.g. --option arg).
+/// - OPT_OTHER: The argument of the option is of another custom type (e.g. a
+/// structure or an array or file with a custom extension etc.) 
+/// - OPT_INT: The argument of the option is an integer
+/// - OPT_STRING: The argument of the option is a string
+/// - OPT_BOOL: The argument of the option is a boolean (0 | 1 | true | false)
+/// - OPT_FLOAT: The argument of the option is a float
+/// - OPT_LONG: The argument of the option is a long
+/// - OPT_DOUBLE: The argument of the option is a double
+/// - OPT_ALPHANUM: The argument of the option is an alphanumeric string
+/// - OPT_HEX: The argument of the option is a hexadecimal number
+/// - OPT_OCT: The argument of the option is an octal number
+/// @details The flags can be combined with the bitwise OR operator '|'.
+/// @details The flags OPT_INT, OPT_STRING, OPT_BOOL, OPT_FLOAT, OPT_LONG,
+/// OPT_DOUBLE, OPT_ALPHANUM, OPT_HEX, OPT_OCT are the standard types that can
+/// be used to define the type of the argument of the option. The flag OPT_OTHER
+/// is a custom type that can be used to define the type of the argument of the
+/// option. The custom type can be defined by setting a custom checker with the
+/// function set_custom_checker. The custom checker will be called to check the
+/// argument of the option. The custom checker should return 0 if the argument
+/// is valid and -1 if the argument is invalid. The custom checker should be
+/// defined as follows:
+/// @code
+/// int	custom_checker(char *arg)
+/// {
+/// // check the argument
+/// return (0 or -1);
+/// }
+/// @endcode
+/// @see: set_custom_checker to set a custom checker for the argument of type
+/// OPT_OTHER.
+/// @warning: System limitations: Currently you cannot set an option which uses
+/// more than one argument. You can set an option which uses a custom type
+/// (OPT_OTHER) but the custom type should be defined as a single argument.
+/// @see: t_opt
+/// @see: set_custom_checker
 ///
-/// the following are the types of the argument that can be parsed:
-/// OPT_INT: The argument is a whole number
-/// OPT_STRING: The argument is a string
-/// OPT_BOOL: The argument is a boolean
-/// OPT_FLOAT: The argument is a decimal number
-/// OPT_OTHER: The argument is another type aka
-///   (your function will recieve the next argument without any checks)
-/// eg. OPT_INT | OPT_SPACE will parse an option with an integer argument
-/// separated by a space from the option identifier : -n 42 || --name 42
-/// eg. OPT_INT | OPT_EQSIGN will parse an option with an integer argument
-/// separated by an equal sign from the option identifier : -n=42 || --name=42
-/// ...
-///
+
 typedef enum e_opt_type
 {
-	OPT_NOARG = 1,
-	OPT_EQSIGN = 2,
-	OPT_NXT = 3,
-	OPT_INT = 4,
-	OPT_STRING = 8,
-	OPT_BOOL = 12,
-	OPT_FLOAT = 16,
-	OPT_OTHER = 20
+	OPT_ARG = 1,// technically the inverse of OPT_NOARG so mask it
+	OPT_EQSIGN = 2,// if it has an arg and not eqsign, it will be the next arg aka space separated
+	OPT_OTHER = 4,// custom type, see set_custom_checker
+	OPT_INT = 8,
+	OPT_STRING = 12,
+	OPT_BOOL = 16,
+	OPT_FLOAT = 20,
+	OPT_LONG = 24,
+	OPT_DOUBLE = 28,
+	OPT_ALPHANUM = 32,
+	OPT_HEX = 36,
+	OPT_OCT = 40,
 }				t_opt_type;
 
 /// @brief Structure to define the options
 /// @param opt_long_name: The long name of the option "name" (without the "--")
 /// @param opt_short_name: The short name of the option 'n' (without the "-")
-/// @param opt_func: The function to call when the option is found
 /// @param opt_type: The type of the option
-/// @details The opt_func function must have the following prototype:
-/// void opt_func(void *control_struct, char *arg)
-/// Where:
-///		The control_struct is a pointer to the structure that your function will
-///		be given.
-///		The arg is the argument of the option is the parsed string found after
-///		the	option identifier.
-///		(It doesnt cast. Only check if string is valid with the type
-///		eg. OPT_INT type must only contain digits to be valid (doe
-///		not check if the number is in the range of an int)).
-///		If the option is a flag (with OPT_NONE eg. -v) the arg will be NULL.
+/// @param opt_func: The function to call when the option is found casted as 
+/// void * and later casted depending on the flags
+///	@details An example of how to use the structure:
+///	@code
+///	#include "ft_args.h"
+///	#include "ft_args_types.h"
+///	#include <stdio.h>
+///
+///	typedef struct s_control
+///	{
+///	int		n;
+///	char	*name;
+///	float	f;
+///	}				t_control;
+///
+///	void	set_n(void *control_struct, char *arg)
+///	{
+///	((t_control *)control_struct)->n = ft_atoi(arg);
+///	}
+///	void	set_name(void *control_struct, char *arg)
+///	{
+///	((t_control *)control_struct)->name = arg;
+///	}
+///	void	set_f(void *control_struct, char *arg)
+///	{
+///	((t_control *)control_struct)->f = ft_atof(arg);
+///	}
+/// int	main(int argc, char **argv)
+/// {
+/// t_control	control;
+/// t_opt		opt_list[] = {
+/// {"nbr", 'n', set_n, OPT_INT | OPT_EQSIGN},
+/// {"name", 'a', set_name, OPT_STRING | OPT_EQSIGN},
+/// {"float", 'f', set_f, OPT_FLOAT | OPT_EQSIGN},
+/// {NULL, 0, NULL, 0}
+/// };
+///
+/// ft_set_opt_list(opt_list);
+/// ft_parse_args(argc, argv, &control);
+/// printf("n = %d\nname = %s\nf = %f\n", control.n, control.name, control.f);
+/// return (0);
+/// }
+/// @endcode
+/// @details The previous code will parse the arguments and set the values of 
+/// the control structure according to the options found in the arguments.
+/// @details the function set_n will be called when the option --nbr or -n is
+/// found in the arguments, and will be casted as a void (*) (void *, char *).
+///
 typedef struct s_opt
 {
 	char		*long_name;
 	char		short_name;
-	void		(*func)(void *control_struct, char *arg);
+	void		*func;
 	t_opt_type	type;
 }				t_opt;
 
--- src/ft_args/ft_parse_args.c
+++ src/ft_args/ft_parse_args.c
@@ -6,22 +6,17 @@
 /*   By: bgoulard <bgoulard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/14 01:10:29 by bgoulard          #+#    #+#             */
-/*   Updated: 2024/05/18 19:45:45 by bgoulard         ###   ########.fr       */
+/*   Updated: 2024/05/21 10:47:48 by bgoulard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_string.h"
 #include "ft_args.h"
 #include "ft_args_types.h"
 #include "ft_math.h"
 #include "ft_string.h"
 #include <sys/types.h>
-
-// better idea: since core comparaison to OPT flags are the same,
-//   get parse_long_opt and parse_short_opt to return the index of the matched
-//   future problem:
-//     problem: can we match -n <string> and a -n <number> ?
-//     technically we can already do that with the flags but we need to get the
-//     string after the concerned string_option...
+#include <stdbool.h>
 
 ssize_t	parse_long_opt(char *str_op, const t_opt *opt_list)
 {
@@ -57,15 +52,95 @@ ssize_t	parse_short_opt(char *str_op, const t_opt *opt_list)
 	return (-1);
 }
 
-void	run_opt_func(const t_opt opt, void *usr_control_struct, char *arg,
-		char *arg_nxt)
+static int	(*singleton_custom_checker(int (*custom_checker)(char *arg)))
+	(char *arg)
 {
-	if (opt.type & OPT_NOARG)
-		opt.func(usr_control_struct, NULL);
-	else if (opt.type & OPT_NXT)
-		opt.func(usr_control_struct, arg_nxt);
-	else if (opt.type & OPT_EQSIGN)
-		opt.func(usr_control_struct, ft_strchr(arg, '='));
+	static int	(*custom_checker_ptr)(char *) = NULL;
+
+	if (custom_checker)
+		custom_checker_ptr = custom_checker;
+	return (custom_checker_ptr);
+}
+
+void	ft_arg_set_custom_checker(int (*custom_checker)(char *))
+{
+	singleton_custom_checker(custom_checker);
+}
+
+int	(*ft_arg_get_custom_checker(void))(char *arg)
+{
+	return (singleton_custom_checker(NULL));
+}
+
+int	ft_isint(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	if (str[i] == '-')
+		i++;
+	while (str[i])
+	{
+		if (!ft_isdigit(str[i]))
+			return (false);
+		i++;
+	}
+	if ((i == 10 && ft_strcmp(str, "2147483647") > 0) || (i == 11
+			&& ft_strcmp(str, "-2147483648") > 0))
+		return (false);
+	return (true);
+}
+// 0 success, !0 failure
+int	checker_arg(t_opt_type type, char *arg)
+{
+	();
+
+	if (!arg)
+		return (type & OPT_ARG); // if arg is NULL, return 1 if OPT_ARG is set otherwise 0
+	if (type & OPT_INT)
+		return (ft_isint(arg));
+	else if (type & OPT_STRING)
+		return (EXIT_SUCCESS);
+	else if (type & OPT_BOOL)
+		return (ft_isbool(arg));
+	else if (type & OPT_FLOAT)
+		return (ft_isfloat(arg));
+	else if (type & OPT_LONG)
+		return (ft_islong(arg));
+	else if (type & OPT_DOUBLE)
+		return (ft_isdouble(arg));
+	else if (type & OPT_ALPHANUM)
+		return (ft_str_isalphanum(arg));
+	else if (type & OPT_HEX)
+		return (ft_ishex(arg));
+	else if (type & OPT_OCT)
+		return (ft_isoct(arg));
+	else if (type & OPT_OTHER && custom_checker)
+		return (custom_checker(arg));
+	return (EXIT_SUCCESS);
+}
+
+int	run_opt_func(const t_opt opt, void *usr_control_struct, char **arg, int *i)
+{
+	char		*arg_ptr;
+	void const	(*func_arg)(void *, char *) = opt.func;
+	void const	(*func_no_arg)(void *) = opt.func;
+
+	if (opt.type & OPT_ARG && opt.type & OPT_EQSIGN)
+		arg_ptr = ft_strchr(arg[*i], '=') + 1;
+	else if (opt.type & OPT_ARG)
+		arg_ptr = arg[*i + 1];
+	else
+		arg_ptr = NULL;
+	if (checker_arg(opt.type, arg_ptr) != 0)
+		return (EXIT_FAILURE);
+	if (opt.type & OPT_ARG)
+	{
+		func_arg(usr_control_struct, arg_ptr);
+		*i += 1 + (opt.type & OPT_EQSIGN) != OPT_EQSIGN;
+	}
+	else
+		func_no_arg(usr_control_struct);
 }
 
 int	ft_parse_args(int argc, char **argv, void *usr_control_struct)
@@ -85,13 +160,12 @@ int	ft_parse_args(int argc, char **argv, void *usr_control_struct)
 			return (EXIT_SUCCESS);
 		else if (argv[i][1] == '-') // aka --XXXXX
 			opt_index = parse_long_opt(argv[i] + 2, opt);
-		else
+		else // aka -X
 			opt_index = parse_short_opt(argv[i] + 1, opt);
 		if (opt_index == -1)
 			return (ft_putstr_fd("Unkown option\n", 2), EXIT_FAILURE);
-		// todo reference the option and prog name ft_putstrerr idk?
-		run_opt_func(opt[opt_index], usr_control_struct, argv[i], argv[i + 1]);
-		i += ((opt[opt_index].type & OPT_NOARG) != 0) + 1;
+		run_opt_func(opt[opt_index], usr_control_struct, argv, &i);
+			
 	}
 	return (EXIT_SUCCESS);
 }
